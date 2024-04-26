b49e8f0e939ccd86bf157905f697467ae8493211
Rookie, LIN <1049825685@qq.com>
2024-04-26 17:54:34 +0800

    :lipstick: 新增tailwind作為css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {},
+  plugins: [],
+}
+
